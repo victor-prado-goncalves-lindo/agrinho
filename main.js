bcae5b08e477b05225ae948312355dbec4b9c1f8
victor-prado-goncalves-lindo <140552623+victor-prado-goncalves-lindo@users.noreply.github.com>
2024-07-31 07:42:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function compre() {
+const width = 800;
+const height = 600;
+const left =(screen.width/2)-(width/ 2);
+const top =(screen.height/2)-(height/ 2);
+window.open('pagamento.html','Pagamento','width=${width},height=${height},top=${top},left=${left}');}
